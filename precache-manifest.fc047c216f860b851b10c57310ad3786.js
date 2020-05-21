2c259a176d092dfebf76ed7f435ff4ed79531ee1
lrmendes <lrmen14@gmail.com>
2020-05-21 20:12:18 +0100

    Updates

---
+++ precache-manifest.fc047c216f860b851b10c57310ad3786.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eb7cfcc26e8074c6d3d7d6e911670a",
+    "url": "/pokemon-graphql/index.html"
+  },
+  {
+    "revision": "1a51e94f4d997543494f",
+    "url": "/pokemon-graphql/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98c46bb42d31f85e8813",
+    "url": "/pokemon-graphql/static/js/2.d68977fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-graphql/static/js/2.d68977fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a51e94f4d997543494f",
+    "url": "/pokemon-graphql/static/js/main.9a4bf41e.chunk.js"
+  },
+  {
+    "revision": "7b57605606af7c5ea99e",
+    "url": "/pokemon-graphql/static/js/runtime-main.a0d49bb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemon-graphql/static/media/logo.5d5d9eef.svg"
+  }
+]);
